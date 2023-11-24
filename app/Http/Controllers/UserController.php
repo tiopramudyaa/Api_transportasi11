2ffb84e48ff9fe4f027c9299d7d1dae65cc9ee05
SamuelJuang <samueljuang007@gmail.com>
2023-11-24 13:30:42 +0700

    UserDone

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        try{
+            $user = User::all();
+            return response()->json([
+                "status" => true,
+                "message" => 'Berhasil ambil Data',
+                "data" => $user
+            ],200);
+        }
+        catch(\Exception $e){
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage(),
+                "data" => []
+            ],400);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        try{
+            $user = User::create($request->all());
+            return response()->json([
+                "status" => true,
+                "message" => 'Berhasil Insert Data',
+                "data" => $user
+            ],200);
+        }
+        catch(\Exception $e){
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage(),
+                "data" => []
+            ],400);
+        }
+    }
+
+    public function show($id)
+    {
+        try{
+            $user = User::find($id);
+
+            if(!$user) throw new \Exception("Barang tidak ditemukan");
+
+            return response()->json([
+                "status" =>true,
+                "message" => 'Berhasil Ambil Data',
+                "data" => $user
+            ],200);
+        }
+        catch(\Exception $e){
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage(),
+                "data" => []
+            ],400);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        try{
+            $user = User::find($id);
+
+            if(!$user) throw new \Exception("Barang tidak ditemukan");
+
+            $user->update($request->all());
+
+            return response()->json([
+                "status" => true,
+                "message" =>'Berhasil Update Data',
+                "data" => $user
+            ],200);
+        }
+        catch(\Exception $e){
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage(),
+                "data" => []
+            ],400);
+        }
+    }
+
+    public function destroy($id)
+    {
+        try{
+            $user = User::find($id);
+
+            if(!$user) throw new \Exception("Barang tidak ditemukan");
+
+            $user->delete();
+
+            return response()->json([
+                "status" => true,
+                "message" =>'Berhasil delete Data',
+                "data" => $user
+            ],200);
+        }
+        catch(\Exception $e){
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage(),
+                "data" => []
+            ],400);
+        }
+    }
+
+    public function authenticate(Request $request){
+        try{
+            $authenticationData = $request->only('username','password');
+            $userLogin = User::where('name',$authenticationData['username'])->where('password',$authenticationData['password'])->first();
+            if(!$userLogin)throw new \Exception("Login Invalid");
+            return response()->json([
+                "status" => true,
+                "message" =>'Berhasil Login',
+                "data" => $userLogin
+            ],200);
+        }catch(\Exception $e){
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage(),
+                "data" => []
+            ],400);
+        }
+    }
+
+    public function resetPassword(Request $request){
+        try{
+            $authenticationData = $request->only('username');
+            $userChange = User::where('name',$authenticationData['username'])->first();
+            if(!$userChange)throw new \Exception("User tidak ditemukan");
+            $userChange->password = $request['password'];
+            return response()->json([
+                "status" => true,
+                "message" =>'Berhasil Mengubah Password',
+                "data" => $userChange
+            ],200);
+        }catch(\Exception $e){
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage(),
+                "data" => []
+            ],400);
+        }
+    }
+    
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -9,37 +9,18 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
-{
+{   
     use HasApiTokens, HasFactory, Notifiable;
+    protected $table = "users";
+    protected $primaryKey = "id";
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
+        'fullName',
+        'noTelp',
         'password',
+        'profilePicture'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
 }
